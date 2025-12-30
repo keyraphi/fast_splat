eb6a52040af70d6253e835f02e8cfb8dcda82805
Raphael Braun <keyraphi@gmail.com>
2025-12-30 19:54:13 +0100

    AoS to SoA

--- src/cpp/cpu/fast_splat_2d_cpu.cpp
+++ src/cpp/cpu/fast_splat_2d_cpu.cpp
@@ -1,7 +1,6 @@
 #include "fast_splat_2d_cpu.h"
-#include <cstddef>
 #include <cmath>
-
+#include <cstddef>
 
 void bilinear_splat_pixel(const float red, const float green, const float blue,
                           const float x, const float y, float *image,
@@ -10,6 +9,7 @@ void bilinear_splat_pixel(const float red, const float green, const float blue,
   int x_right = x_left + 1;
   int y_top = static_cast<int>(std::floor(y));
   int y_bottom = y_top + 1;
+  size_t pixel_count = width * height;
 
   if (x_left >= 0 && x_left < static_cast<int>(width)) {
     const float weight_left = static_cast<float>(x_right) - x;
@@ -17,22 +17,27 @@ void bilinear_splat_pixel(const float red, const float green, const float blue,
       const float weight_top = static_cast<float>(y_bottom) - y;
       const float weight_top_left = weight_left * weight_top;
 #pragma omp atomic
-      image[y_top * width * 3 + static_cast<size_t>(x_left) * 3 + 0] += weight_top_left * red;
+      image[y_top * width + static_cast<size_t>(x_left)] +=
+          weight_top_left * red;
 #pragma omp atomic
-      image[y_top * width * 3 + static_cast<size_t>(x_left) * 3 + 1] += weight_top_left * green;
+      image[y_top * width + static_cast<size_t>(x_left) + pixel_count] +=
+          weight_top_left * green;
 #pragma omp atomic
-      image[y_top * width * 3 + static_cast<size_t>(x_left) * 3 + 2] += weight_top_left * blue;
+      image[y_top * width + static_cast<size_t>(x_left) + 2 * pixel_count] +=
+          weight_top_left * blue;
     }
     if (y_bottom >= 0 && y_bottom < static_cast<int>(height)) {
       const float weight_bottom = y - static_cast<float>(y_top);
       const float weight_bottom_left = weight_left * weight_bottom;
 #pragma omp atomic
-      image[y_bottom * width * 3 + static_cast<size_t>(x_left) * 3 + 0] += weight_bottom_left * red;
+      image[y_bottom * width + static_cast<size_t>(x_left)] +=
+          weight_bottom_left * red;
 #pragma omp atomic
-      image[y_bottom * width * 3 + static_cast<size_t>(x_left) * 3 + 1] +=
+      image[y_bottom * width + static_cast<size_t>(x_left) + pixel_count] +=
           weight_bottom_left * green;
 #pragma omp atomic
-      image[y_bottom * width * 3 + static_cast<size_t>(x_left) * 3 + 2] += weight_bottom_left * blue;
+      image[y_bottom * width + static_cast<size_t>(x_left) + 2 * pixel_count] +=
+          weight_bottom_left * blue;
     }
   }
   if (x_right >= 0 && x_right < static_cast<int>(width)) {
@@ -41,23 +46,26 @@ void bilinear_splat_pixel(const float red, const float green, const float blue,
       const float weight_top = static_cast<float>(y_bottom) - y;
       const float weight_top_right = weight_right * weight_top;
 #pragma omp atomic
-      image[y_top * width * 3 + static_cast<size_t>(x_right) * 3 + 0] += weight_top_right * red;
+      image[y_top * width + static_cast<size_t>(x_right) ] +=
+          weight_top_right * red;
 #pragma omp atomic
-      image[y_top * width * 3 + static_cast<size_t>(x_right) * 3 + 1] += weight_top_right * green;
+      image[y_top * width + static_cast<size_t>(x_right) + pixel_count] +=
+          weight_top_right * green;
 #pragma omp atomic
-      image[y_top * width * 3 + static_cast<size_t>(x_right) * 3 + 2] += weight_top_right * blue;
+      image[y_top * width + static_cast<size_t>(x_right) + 2*pixel_count] +=
+          weight_top_right * blue;
     }
     if (y_bottom >= 0 && y_bottom < static_cast<int>(height)) {
       const float weight_bottom = y - static_cast<float>(y_top);
       const float weight_bottom_right = weight_right * weight_bottom;
 #pragma omp atomic
-      image[y_bottom * width * 3 + static_cast<size_t>(x_right) * 3 + 0] +=
+      image[y_bottom * width + static_cast<size_t>(x_right) ] +=
           weight_bottom_right * red;
 #pragma omp atomic
-      image[y_bottom * width * 3 + static_cast<size_t>(x_right) * 3 + 1] +=
+      image[y_bottom * width + static_cast<size_t>(x_right) * pixel_count] +=
           weight_bottom_right * green;
 #pragma omp atomic
-      image[y_bottom * width * 3 + static_cast<size_t>(x_right) * 3 + 2] +=
+      image[y_bottom * width + static_cast<size_t>(x_right) + 2*pixel_count] +=
           weight_bottom_right * blue;
     }
   }
@@ -72,6 +80,7 @@ void fast_splat_2d_cpu_impl(const float *__restrict__ patch_list,
                             const size_t target_height) {
   float patch_radius_x = static_cast<float>(patch_width) / 2.F;
   float patch_radius_y = static_cast<float>(patch_height) / 2.F;
+  const size_t patch_pixel_count = patch_width*patch_height;
 
 #pragma omp parallel for
   for (size_t i = 0; i < patch_count; ++i) {
@@ -83,15 +92,16 @@ void fast_splat_2d_cpu_impl(const float *__restrict__ patch_list,
     const float start_pos_y = pos_y - patch_radius_y;
     for (size_t y = 0; y < patch_height; y++) {
       for (size_t x = 0; x < patch_width; x++) {
-        const float red = patch[y * patch_width * 3 + x * 3 + 0];
-        const float green = patch[y * patch_width * 3 + x * 3 + 1];
-        const float blue = patch[y * patch_width * 3 + x * 3 + 2];
+        const float red = patch[y * patch_width + x];
+        const float green =
+            patch[y * patch_width + x + 1 * patch_pixel_count];
+        const float blue =
+            patch[y * patch_width + x + 2 * patch_pixel_count];
         const float pixel_pos_x = start_pos_x + static_cast<float>(x);
         const float pixel_pos_y = start_pos_y + static_cast<float>(y);
         bilinear_splat_pixel(red, green, blue, pixel_pos_x, pixel_pos_y, result,
-                       target_width, target_height);
+                             target_width, target_height);
       }
     }
   }
 }
-
