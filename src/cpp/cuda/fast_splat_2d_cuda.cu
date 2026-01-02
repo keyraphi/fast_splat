9a80ca7d0cc60ed15d83d575ca649ad43e582f7c
Raphael Braun <keyraphi@gmail.com>
2026-01-02 14:58:09 +0100

    fix

--- src/cpp/cuda/fast_splat_2d_cuda.cu
+++ src/cpp/cuda/fast_splat_2d_cuda.cu
@@ -219,8 +219,9 @@ __global__ void fast_splat_2d_kernel(
   // iterate over all patches that need to be splatet into this tile
   uint32_t patches_for_this_tile = patches_per_tile[tile_id];
   uint32_t patch_index_offsets_for_this_tile = tile_index_offsets[tile_id];
-  if (threadIdx== 0) {
-    printf("patches for this tile: %u, offset: %u\n", patches_for_this_tile, patch_index_offsets_for_this_tile);
+  if (threadIdx.x == 0) {
+    printf("patches for this tile: %u, offset: %u\n", patches_for_this_tile,
+           patch_index_offsets_for_this_tile);
   }
   for (uint32_t i = 0; i < patches_for_this_tile; i++) {
     uint32_t patch_id = indices[patch_index_offsets_for_this_tile + i];
@@ -233,7 +234,9 @@ __global__ void fast_splat_2d_kernel(
     size_t patch_based_idx =
         static_cast<size_t>(patch_id) * patch_width * patch_height * 3;
     if (tile_id == 0 && threadIdx.x == 0) {
-      printf("tile %lu, patch %u: (%f, %f), (%f, %f)\n", tile_id, patch_id, patch_center_pos_x, patch_center_pos_y, patch_left_in_tile, patch_top_in_tile);
+      printf("tile %lu, patch %u: (%f, %f), (%f, %f)\n", tile_id, patch_id,
+             patch_center_pos_x, patch_center_pos_y, patch_left_in_tile,
+             patch_top_in_tile);
     }
     // attomicly add the pixels of this patch to this tile at the correct
     // positions using bilinear interpolation
